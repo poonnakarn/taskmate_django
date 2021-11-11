f64e8703a33bcd706bbb34234a880bc104b47154
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-11 21:47:34 +0700

    Add some bootstrap to form

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
+from django.contrib import messages
 
 # Create your views here.
-
-
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
+        messages.success(request, ("New Task Added!"))
         return redirect("todolist")
     else:
         all_tasks = TaskList.objects.all
