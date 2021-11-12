9b63cac057a3200695cb79f73ada0f0411bbe0a6
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-12 11:15:52 +0700

    Add delete functionality

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -17,6 +17,12 @@ def todolist(request):
         return render(request, "todolist.html", {"all_tasks": all_tasks})
 
 
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect("todolist")
+
+
 def contact(request):
     context = {
         "welcome_text": "Welcome to Contact Page.",
