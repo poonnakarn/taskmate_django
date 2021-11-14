f4df21e9b3adc9ac41bee1b330946aaedc6106e3
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-14 19:38:29 +0700

    Add users app

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", todolist_views.index, name="index"),
     path("todolist/", include("todolist_app.urls")),
+    path("account/", include("users_app.urls")),
     path("contact", todolist_views.contact, name="contact"),
     path("about-us", todolist_views.about, name="about"),
 ]
