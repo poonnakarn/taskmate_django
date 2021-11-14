8f9aa19b2059fbc8b668adf854719e1806ddcdb7
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-14 20:50:27 +0700

    Add forms.py with CustomRegisterForm class

--- users_app/views.py
+++ users_app/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomRegisterForm
 from django.contrib import messages
 
 
 def register(request):
     if request.method == "POST":
         # POST
-        register_form = UserCreationForm(request.POST)
+        register_form = CustomRegisterForm(request.POST)
         if register_form.is_valid():
             # form valid
             register_form.save()
             messages.success(
                 request, ("New User Account Created, Login To Get Started!")
             )
-            return redirect("todolist")
+            return redirect("register")
     else:
         # GET
-        register_form = UserCreationForm()
+        register_form = CustomRegisterForm()
     return render(request, "register.html", {"register_form": register_form})
