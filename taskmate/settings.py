3e7a2b6389b3029be8d2f9a09bf11d5e77f98d05
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-14 21:19:09 +0700

    Filter with Crispy Form

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "todolist_app",
     "users_app",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
